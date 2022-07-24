219da047a5d3eaa7228563fdd465cfa6e7c4b18e
Alhan.K <alhan.kaypour@gmail.com>
2022-07-24 22:10:42 +0430

    few changes added

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("dotenv").config();
-
 var app = express();
+const userRouter = require("./src/module/users/router");
+const AuthMiddleware = require("./src/core/middleware/auth");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use("/users", userRouter);
+app.post("/login", AuthMiddleware.login);
 module.exports = app;
