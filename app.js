61c09ce4ad879910e310df81bd4bc17efe48800d
Alhan.K <alhan.kaypour@gmail.com>
2022-07-31 00:09:38 +0430

    installed "cors" package to handle the requests from another domain

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("dotenv").config();
+var cors = require("cors");
+
 var app = express();
 const userRouter = require("./src/module/users/router");
 const AuthMiddleware = require("./src/core/middleware/auth");
 
+app.use(cors({ origin: "*", methods: "GET,HEAD,PUT,PATCH,POST,DELETE" }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
