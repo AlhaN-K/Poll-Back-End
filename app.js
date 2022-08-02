1c728e53d9052c76168b512fea94a72c4be8c237
Alhan.K <alhan.kaypour@gmail.com>
2022-08-02 23:01:22 +0430

    poll route created

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("dotenv").config();
 var cors = require("cors");
-
 var app = express();
+
 const userRouter = require("./src/module/users/router");
+const pollRouter = require("./src/module/polls/router");
 const AuthMiddleware = require("./src/core/middleware/auth");
 
 app.use(cors({ origin: "*", methods: "GET,HEAD,PUT,PATCH,POST,DELETE" }));
@@ -16,5 +17,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/users", userRouter);
+app.use("/polls", pollRouter);
 app.post("/login", AuthMiddleware.login);
 module.exports = app;
