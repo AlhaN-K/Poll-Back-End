4292cbc2e2b59e813769ed328beb0c75f800b48b
Alhan.K <alhan.kaypour@gmail.com>
2022-08-09 14:39:51 +0430

    added validator to create poll route

--- src/module/polls/router.js
+++ src/module/polls/router.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const PollController = require("./controller");
+const PollValidator = require("./validation");
 
 router.get("/", PollController.getPolls);
 router.get("/:id", PollController.getPollById);
-router.post("/", PollController.createPoll);
+router.post("/", PollValidator.createPollSchema, PollController.createPoll);
 router.patch("/:id", PollController.updatePoll);
 router.delete("/:id", PollController.deletePoll);
 
