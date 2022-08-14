258e1349d60bda67f07d504da81c96b0bae51002
Alhan.K <alhan.kaypour@gmail.com>
2022-08-14 16:56:34 +0430

    removed user id from validation controller

--- src/module/polls/validation/index.js
+++ src/module/polls/validation/index.js
@@ -4,7 +4,6 @@ class PollValidator {
   static createPollSchema = celebrate({
     [Segments.BODY]: Joi.object().keys({
       title: Joi.string().max(64).required(),
-      user_id: Joi.number().integer().required(),
       description: Joi.string().max(256).required(),
       link: Joi.string().required(),
     }),
