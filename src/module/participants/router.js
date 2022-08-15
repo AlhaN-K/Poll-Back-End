1bd7d884946a99e0d45a501344f35b911c2f5b2b
Alhan.K <alhan.kaypour@gmail.com>
2022-08-15 16:39:28 +0430

    participant router added

---
+++ src/module/participants/router.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const ParticipantController = require("./controller");
+const ParticipantValidator = require("./validation");
+const AuthMiddleware = require("../../core/middleware/auth");
+
+router.get(
+  "/",
+  AuthMiddleware.jwtTokenValidation,
+  ParticipantController.getParticipants
+);
+router.get(
+  "/:id",
+  AuthMiddleware.jwtTokenValidation,
+  ParticipantController.getParticipantById
+);
+router.post(
+  "/",
+  AuthMiddleware.jwtTokenValidation,
+  ParticipantValidator.createParticipantSchema,
+  ParticipantController.createParticipant
+);
+router.patch(
+  "/:id",
+  AuthMiddleware.jwtTokenValidation,
+  ParticipantController.updateParticipant
+);
+router.delete(
+  "/:id",
+  AuthMiddleware.jwtTokenValidation,
+  ParticipantController.deleteParticipant
+);
+module.exports = router;
