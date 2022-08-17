089e508f0a548e624a0f0a37eb7805677a499a68
Alhan.K <alhan.kaypour@gmail.com>
2022-08-17 19:13:17 +0430

    choice router added

---
+++ src/module/choices/router.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const ChoiceController = require("./controller");
+const AuthMiddleware = require("../../core/middleware/auth");
+const ChoiceValidator = require("./validation/index");
+
+router.get("/", AuthMiddleware.jwtTokenValidation, ChoiceController.getChoices);
+router.get(
+  "/:id",
+  AuthMiddleware.jwtTokenValidation,
+  ChoiceController.getChoiceById
+);
+router.post(
+  "/",
+  AuthMiddleware.jwtTokenValidation,
+  ChoiceValidator.createChoiceSchema,
+  ChoiceController.createChoice
+);
+
+module.exports = router;
