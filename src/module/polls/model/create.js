4c0e16ed0626b0e943972144617826479ffb6016
Alhan.K <alhan.kaypour@gmail.com>
2022-08-17 19:08:13 +0430

    link deleted from poll table

--- src/module/polls/model/create.js
+++ src/module/polls/model/create.js
@@ -2,11 +2,11 @@ const DatabaseManager = require("../../../core/database/databaseManager");
 
 class PollCreator {
   static createPoll(userId, pollData) {
-    const { title, description, link } = pollData;
+    const { title, description } = pollData;
     const create = `INSERT INTO poll 
-    (title, user_id, description, link)  
+    (title, user_id, description)  
     VALUES
-    ('${title}','${userId}','${description}','${link}');`;
+    ('${title}',${userId},'${description}');`;
     return DatabaseManager.query(create);
   }
 }
--- src/module/polls/model/update.js
+++ src/module/polls/model/update.js
@@ -2,11 +2,11 @@ const DatabaseManager = require("../../../core/database/databaseManager");
 
 class PollEditor {
   static updatePoll(pollId, pollData) {
-    const { title, user_id, description, link } = pollData;
+    const { title, user_id, description } = pollData;
     const update = `
     UPDATE poll 
     SET
-    title ='${title}', user_id ='${user_id}', description ='${description}', link ='${link}'
+    title ='${title}', user_id =${user_id}, description ='${description}'
     WHERE ID = ${pollId};`;
     return DatabaseManager.query(update);
   }
