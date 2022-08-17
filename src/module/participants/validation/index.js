d1d369eba6434c480dec7d683c6f65ff5c0e1de9
Alhan.K <alhan.kaypour@gmail.com>
2022-08-17 19:09:10 +0430

    added poll id validation to participant

--- src/module/participants/validation/index.js
+++ src/module/participants/validation/index.js
@@ -3,6 +3,7 @@ const { celebrate, Joi, Segments } = require("celebrate");
 class ParticipantValidator {
   static createParticipantSchema = celebrate({
     [Segments.BODY]: Joi.object().keys({
+      poll_id: Joi.number().integer().required(),
       name: Joi.string().max(32).required(),
     }),
   });
