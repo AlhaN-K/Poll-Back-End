9de512a84adba7c7544fd13cb4b0f94b8a3a6865
Alhan.K <alhan.kaypour@gmail.com>
2022-08-20 22:37:28 +0430

    added /api/ to app.js routes

--- app.js
+++ app.js
@@ -9,25 +9,25 @@ var app = express();
 
 const userRouter = require("./src/module/users/router");
 const pollRouter = require("./src/module/polls/router");
-const itemsRouter = require("./src/module/items/router");
+const itemsRouter = require("./src/module/Items/router");
 const participantRouter = require("./src/module/participants/router");
 const choicesRouter = require("./src/module/choices/router");
 const AuthMiddleware = require("./src/core/middleware/auth");
 
-app.use(cors({ origin: "*", methods: "GET,HEAD,PUT,PATCH,POST,DELETE" }));
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/users", userRouter);
-app.use("/polls", pollRouter);
-app.use("/pollItems", itemsRouter);
-app.use("/participants", participantRouter);
-app.use("/choices", choicesRouter);
+app.use("/api/users", userRouter);
+app.use("/api/polls", pollRouter);
+app.use("/api/pollItems", itemsRouter);
+app.use("/api/participants", participantRouter);
+app.use("/api/choices", choicesRouter);
 
-app.post("/login", AuthMiddleware.login);
+app.post("/api/login", AuthMiddleware.login);
 
 app.use(errors());
 
